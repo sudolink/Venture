ab0dada23bff5ae52b0c90d9dc01ab7b52efc310
sudolink <tilen@tutanota.com>
2019-01-11 19:45:29 +0100

    Modified how the alpha game map displays with some list-comp 'magic'. Started work on map objects taking items on init and defining their 'portals' to other maps based on their position in the map matrix

--- maps.py
+++ maps.py
@@ -16,20 +16,28 @@ map_desc = ["A cold and dark cavern","A deserted kitchen","A dusty bedroom with 
 class maps():
 	'Maps have containers for items and beings. They can have effects on their occupants'
 
-	def __init__(self, description):
+	def __init__(self, description,coords):
 		self.description = description
 		self.items = {} #items in area
 		self.occupants = []	#creature objects residing in map
+		self.coords = coords
+		self.visited = False
+		self.hasPlayer = False
+		self.exits = []
 
 	def acceptPlayer(self,player):
-		try:
-			self.occupants.append(player)
-		except:
-			print("Something went awry!")
-		else:
-			print("{} enters.".format(self.occupants[0].name))
-			####this works during testing only!
-			####(self.occupants[0].name) because only occupant
+		self.occupants.append(player)
+		self.hasPlayer = True
+		if self.visited == False:
+			self.visited = True
+		print("{} enters.".format(self.occupants[0].name))####(self.occupants[0].name) because only occupant
+		self.describeYourself()
+
+	def givePlayer(self):
+		#send player to other map
+		#remove player from occupants list
+		#set hasplayer to false
+		pass
 
 	def takeItems(self,items):
 		if type(items) == list:
@@ -74,42 +82,63 @@ class gameField():
 		self.currentMap = None
 		self.numberOfMaps = random.randint(5,25)
 		self.allmapcoords = []
+		self.allmaps = []
 		########################################
 		##	gamefield lists
 		######
 		self.makeGrid() #generates quadrant lists
 
-		self.showGrid("coords") #show gameMap
+		#self.showGrid("coords") #show gameMap
 
 		#generate maps here
 		self.generateMap()
 		self.showGrid("slots")
+
+		#testing if maps got their items
+		print("______________________________________")
+		print("\nDid the all maps get items properly?\n")
+		for x in self.allmaps:
+			print("{}\n{}".format(x.description,[x for x in x.items.keys()]))
+			print("")
+		print("______________________________________")
+
+
+	def fillMap(self,map_instance):
+		map_instance.takeItems(item.itemPopulator3000())
+		#populate with creatures
 	
 	def generateMap(self):
 		#make genesis here, it inserts itself randomly into the map matrix
 		self.genesisMap("placeholder")
-		print("\nGenesis location: {}".format(self.allmapcoords[0]))
+		#print("\nGenesis location: {}".format(self.allmapcoords[0]))
 
 		#append other maps below
 		self.numberOfMaps = int(self.numberOfMaps / 2) #to allow for empty slots in gameField.
-		print("\nMaps to append: {}".format(self.numberOfMaps))
+		#print("\nMaps to append: {}".format(self.numberOfMaps))
 
 		#run map_placement until numberofmaps is depleted
 		for map_placement in range(self.numberOfMaps):
 			#until map is placed, keep trying
 			notPlaced = True
 			while notPlaced:
+				#pick a random position of row and coord in that row
 				randomrow = random.choice([row for row in self.__dict__ if "row" in row])
 				randomcoord = random.choice([coord for coord in self.__dict__[randomrow]])
 				#are map coordinates already in use?
 				if (randomrow,randomcoord) not in self.allmapcoords:
-					#check if new random coords are adjacent to any other
+					#check if new random coords are adjacent to any already placed map
 					if self.checkIfAdjacent((randomrow,randomcoord)):
-						#then place new map at position
-						makeMap = maps(map_desc.pop(-1))
-						self.__dict__[randomrow][randomcoord] = "HERE"
+							#then make map
+						makeMap = maps(map_desc.pop(-1),(randomrow,randomcoord))
+							#init maps here
+						self.fillMap(makeMap)
+							#place map in matrix
+						self.__dict__[randomrow][randomcoord] = makeMap
+							#put the newly used coords in the list
 						self.allmapcoords.append((randomrow,randomcoord))
-						print("MAP PLACED @ {}-{}".format(randomrow,randomcoord))
+							#put the map in a list
+						self.allmaps.append(makeMap)
+						#print("MAP PLACED @ {}-{}".format(randomrow,randomcoord))
 						notPlaced = False
 					else:
 						#print("Coords not adjacent to any other map!")
@@ -118,7 +147,7 @@ class gameField():
 					#print("Map coordinates already in use!")
 					continue
 
-		print("\n*done appending maps*")
+		#print("\n*done appending maps*")
 
 
 	def checkIfAdjacent(self,location):
@@ -155,7 +184,7 @@ class gameField():
 
 
 	def makeGrid(self):
-		print("Map slots to create: {}".format(self.numberOfMaps))
+		#print("Map slots to create: {}".format(self.numberOfMaps))
 		self.grid_size = None #(number of lists and slots, times to append additional slots)
 		leftover = None
 		#determine grid parameters here
@@ -163,29 +192,25 @@ class gameField():
 			if num * num <= self.numberOfMaps:
 				self.grid_size = num
 				leftover = self.numberOfMaps - (num*num)
-				print("Grid size:{}x{} Leftover is: {}".format(num,num,leftover))
+				#print("Grid size:{}x{} Leftover is: {}".format(num,num,leftover))
 
 				#highest num whose (num*num) result fits into self.numberOfMaps
 				#break, because we've found the best candidate
 				break
 
 		##GRID BUILDING BELOW
-		#making the object attributes
+			#making the object attributes
 		for i in range(self.grid_size):
 			setattr(self,"row{}".format(i),{})
 
-		#some quality of life improvements, definitions for easier use and testing purposes below
+			#some quality of life improvements, definitions for easier use and testing purposes below
 		list_of_rows = [row for row in dir(self) if "row" in row]
 		dictofrows= {k:v for k,v in self.__dict__.items() if "row" in k}
 
-		#print(list_of_rows,"\n",dictofrows)
-		
-		    ###Gridmaking here
 		coordinate_names = ["c{}".format(coordinate) for coordinate in range(self.numberOfMaps)] #list of all coordinates for this gamefield c0-c99
-		#dictofrows["row0"]["c0"] = "placeholder"
 
 		row = 0 #start on row 0
-		for coordinate in coordinate_names:
+		for coordinate in coordinate_names: #Goes through rows, appending leftover map slots however many times needed
 			dictofrows["row{}".format(row)][coordinate] = "0"
 			row +=1 #increment to next row
 			if row > len(list_of_rows)-1: #when row number > number of object's row{x} attributes, reset to 0
@@ -204,11 +229,17 @@ class gameField():
 		elif switch == "slots":
 			#this one prints values - coordinate contents (where the maps are inserted)
 			for y in rows.values():
-				print("{} \t {}".format(y,[x for x in y.values()]))
+				print("\n") #breaks after each row  # @'s are maps, ' 's are empty slots
+				for room in ["x" if type(x) == maps else " " for x in y.values()]:
+					print("{}  ".format(room),end="")
+				#print("{} \t {}".format(y,[x for x in y.values()]))
+			print("\n\n")
 		else:
 			print("No display mode specified!")
 		print()
 
+		#for x,y in ((area.description,area.coords) for area in self.allmaps):
+		#	print("{}\t{}".format(y,x))
 
 		#SET UP AS IT IS FOR TESTING PURPOSES
 		#YOU'LL HAVE TO MODIFY THIS TO ACCESS THE MAP OBJECT'S NAME?
@@ -218,8 +249,13 @@ class gameField():
 
 
 	def genesisMap(self,player1):
-		genesis = maps(map_desc.pop(-1))
+			#pick a completely random position for the genesis map
 		randomrow = random.choice([row for row in self.__dict__ if "row" in row])
 		randomcoord = random.choice([coord for coord in self.__dict__[randomrow]])
-		self.__dict__[randomrow][randomcoord] = "genesis"#genesis
+			#take a random map description, and remove it from list; tell the map where it's located
+		genesis = maps(map_desc.pop(-1),(randomrow,randomcoord))
+			#if "genesis" is passed to itempopulator3k then at least one food item will be put in map
+		genesis.takeItems(item.itemPopulator3000("Genesis"))
+		self.__dict__[randomrow][randomcoord] = genesis
 		self.allmapcoords.append((randomrow,randomcoord))
+		self.allmaps.append(genesis)
